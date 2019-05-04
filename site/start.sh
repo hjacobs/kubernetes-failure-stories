2ef500c1407c0ba0ea1abcd5bca1ebf525a0e639
Henning Jacobs <henning@zalando.de>
2019-05-04 20:15:05 +0200

    copy favicon

--- site/start.sh
+++ site/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp favicon.png /var/www/k8s.af/workdir/
+
 IMAGE=kubernetes-failure-stories
 docker build -t $IMAGE .
 docker run -d --name kubernetes-failure-stories -u $(id -u) -v /var/www/k8s.af/workdir:/workdir --restart=always $IMAGE
