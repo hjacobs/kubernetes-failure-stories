89f254783afcac62ef262fedf4b9ee9e720d144e
Henning Jacobs <henning@zalando.de>
2020-08-23 12:42:24 +0200

    sync from codeberg.org

--- site/update.py
+++ site/update.py
@@ -6,18 +6,19 @@ import time
 
 from pathlib import Path
 
-with open(Path(__file__).parent / 'template.html') as fd:
-    template = fd.read()
+template = (Path(__file__).parent / "template.html").read_text()
 
 while True:
-    response = requests.get('https://raw.githubusercontent.com/hjacobs/kubernetes-failure-stories/master/README.md', timeout=5)
+    response = requests.get(
+        "https://codeberg.org/hjacobs/kubernetes-failure-stories/raw/branch/master/README.md",
+        timeout=5,
+    )
     response.raise_for_status()
 
     html = markdown.markdown(response.text)
 
-    out = template.replace('{{content}}', html)
+    out = template.replace("{{content}}", html)
 
-    with open('index.html', 'w') as fd:
-        fd.write(out)
+    Path("index.html").write_text(out)
 
     time.sleep(300)
